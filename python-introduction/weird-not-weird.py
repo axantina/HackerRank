e1864b7f9eaee8399896354bcf9c0db407b760e3
axantina <juliet28_ac@yahoo.com>
2026-06-29 15:25:32 +0900

    Add the command

--- python-introduction/weird-not-weird.py
+++ python-introduction/weird-not-weird.py
@@ -1,3 +1,15 @@
+"""
+HackerRank - Python If-Else
+
+Task:
+Given an integer n, print "Weird" or "Not Weird" based on the following rules:
+
+- If n is odd, print "Weird"
+- If n is even and in the inclusive range of 2 to 5, print "Not Weird"
+- If n is even and in the inclusive range of 6 to 20, print "Weird"
+- If n is even and greater than 20, print "Not Weird"
+"""
+
 n = int(input())
 
 if n % 2 == 1:
