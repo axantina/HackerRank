71c88e1a1cebcc00bbfd653e308ba0f63cfc6db9
axantina <juliet28_ac@yahoo.com>
2026-06-29 15:18:16 +0900

    Add arithmetic operators solution

---
+++ python-introduction/arithmetic_operators.py
@@ -0,0 +1,24 @@
+"""
+HackerRank - Arithmetic Operators
+
+Task:
+Read two integers and print their sum, difference, and product.
+
+Example:
+Input:
+3
+2
+
+Output:
+5
+1
+6
+"""
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    print(a + b)
+    print(a - b)
+    print(a * b)
